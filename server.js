ee728ae61920262041e215aeeeda8555d34da022
kaelcao <aquancva@gmail.com>
2016-08-27 19:11:23 +0000

    completed

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 var express = require("express");
 var app = express();
+var path = require("path");
 
-app.get('/', (req,res) => {
-    res.send('hello world');
+var timeHelper = require("../utils/timeHelper");
+
+app.set('view engine','jade');
+app.set('views', path.join(__dirname,"templates"));
+
+
+app.get('/:time?', (req,res) => {
+    var timeStr = req.params.time;
+    if (timeStr){
+        var unixTime = +timeStr;
+        var time = new Date(timeStr);
+        if (unixTime){
+            time = new Date(+timeStr);
+        }
+        
+        res.json({
+           unix: time.getTime(),
+           natural:timeHelper.formatDate(time,"MMMM d, yyyy")
+        });
+    } else {
+        res.render('index');
+    }
 });
 
 app.listen(8080,() => {
